1fb37643764aafb2d45bcce65af788add1967bd4
Dmitro Skochinskiy <SkochinskiyDmitro@gmail.com>
2016-05-05 13:56:37 +0300

    redesigned front-end

--- department/static/department/js/init.js
+++ department/static/department/js/init.js
@@ -1,13 +1,77 @@
 (function($){
     $(function(){
+        /*
         ajax_cards_list_after('#advertisments-start');
         ajax_divs_list_after('#facts-start');
         ajax_news_cards_list_after('#news-start');
         ajax_partner_carousel_items_list_append('#carousel-items-list-container');
         ajax_technology_divs_list_after('#technologies-start');
+        */
         $('.dropdown-button').dropdown();
-        $('.button-collapse').sideNav();
-        $('.slider').slider({indicators:false});
-        $('.carousel').carousel({dist: 0});
+    $('.button-collapse').sideNav();
+    $('.slider').slider();
+	$('.carousel').carousel({dist: 0});
+	var ctx1 = $("#fact-chart-area1")[0].getContext('2d');
+	var data1 = {
+	    labels: [
+	        "Arg1",
+	        "Arg2",
+	        "Arg3"
+	    ],
+	    datasets: [
+	        {
+	            data: [30, 50, 20],
+	            backgroundColor: [
+	                "#057dc8",
+	                "#d9e7ef",
+	                "#063c5e"
+	            ],
+	            hoverBackgroundColor: [
+	                "#057dc8",
+	                "#d9e7ef",
+	                "#063c5e"
+	            ]
+	        }
+	    ]
+	};
+	Chart.defaults.global.legend.display = false;
+	var fact1 = new Chart(ctx1, {
+	    type: 'doughnut',
+	    data: data1,
+	    options: {
+            responsive: true
+        }
+	});
+
+	var ctx2 = $("#fact-chart-area2")[0].getContext('2d');
+	var data2 = {
+	    labels: [
+	        "Arg1",
+	        "Arg2",
+	        "Arg3"
+	    ],
+	    datasets: [
+	        {
+	            data: [30, 50, 20],
+	            backgroundColor: [
+	                "#057dc8",
+	                "#d9e7ef",
+	                "#063c5e"
+	            ],
+	            hoverBackgroundColor: [
+	                "#057dc8",
+	                "#d9e7ef",
+	                "#063c5e"
+	            ]
+	        }
+	    ]
+	};
+	var fact2 = new Chart(ctx2, {
+	    type: 'doughnut',
+	    data: data2,
+	    options: {
+            responsive: true
+        }
+	});
     }); // end of document ready
 })(jQuery); // end of jQuery name space
