3428099767f498c6e754977028e72e644464909d
projero <ukrainetsua@gmail.com>
2016-05-06 11:24:12 +0300

    Main page fornt-end

--- department/static/department/js/init.js
+++ department/static/department/js/init.js
@@ -6,70 +6,73 @@
         ajax_partner_carousel_items_list_append('#partners-carousel');
         //ajax_technology_divs_list_after('#technologies-start');
         $('.dropdown-button').dropdown();
-    $('.button-collapse').sideNav();
-    $('.slider').slider();
-	var ctx1 = $("#fact-chart-area1")[0].getContext('2d');
-	var data1 = {
-	    labels: [
-	        "Arg1",
-	        "Arg2",
-	        "Arg3"
-	    ],
-	    datasets: [
-	        {
-	            data: [30, 50, 20],
-	            backgroundColor: [
-	                "#057dc8",
-	                "#d9e7ef",
-	                "#063c5e"
-	            ],
-	            hoverBackgroundColor: [
-	                "#057dc8",
-	                "#d9e7ef",
-	                "#063c5e"
-	            ]
-	        }
-	    ]
-	};
-	Chart.defaults.global.legend.display = false;
-	var fact1 = new Chart(ctx1, {
-	    type: 'doughnut',
-	    data: data1,
-	    options: {
-            responsive: true
-        }
-	});
+	    $('.button-collapse').sideNav();
+	    $('.slider').slider();
 
-	var ctx2 = $("#fact-chart-area2")[0].getContext('2d');
-	var data2 = {
-	    labels: [
-	        "Arg1",
-	        "Arg2",
-	        "Arg3"
-	    ],
-	    datasets: [
-	        {
-	            data: [30, 50, 20],
-	            backgroundColor: [
-	                "#057dc8",
-	                "#d9e7ef",
-	                "#063c5e"
-	            ],
-	            hoverBackgroundColor: [
-	                "#057dc8",
-	                "#d9e7ef",
-	                "#063c5e"
-	            ]
+		var ctx1 = $("#fact-chart-area1")[0].getContext('2d');
+		var data1 = {
+		    labels: [
+		        "Arg1",
+		        "Arg2",
+		        "Arg3"
+		    ],
+		    datasets: [
+		        {
+		            data: [30, 50, 20],
+		            backgroundColor: [
+		                "#057dc8",
+		                "#d9e7ef",
+		                "#063c5e"
+		            ],
+		            hoverBackgroundColor: [
+		                "#057dc8",
+		                "#d9e7ef",
+		                "#063c5e"
+		            ]
+		        }
+		    ]
+		};
+		Chart.defaults.global.legend.display = false;
+		var fact1 = new Chart(ctx1, {
+		    type: 'doughnut',
+		    data: data1,
+		    options: {
+	            responsive: true
 	        }
-	    ]
-	};
-	var fact2 = new Chart(ctx2, {
-	    type: 'doughnut',
-	    data: data2,
-	    options: {
-            responsive: true
-        }
-	});
-		$('.carousel').carousel({dist: 0});
+		});
+
+		var ctx2 = $("#fact-chart-area2")[0].getContext('2d');
+		var data2 = {
+		    labels: [
+		        "Arg1",
+		        "Arg2",
+		        "Arg3"
+		    ],
+		    datasets: [
+		        {
+		            data: [30, 50, 20],
+		            backgroundColor: [
+		                "#057dc8",
+		                "#d9e7ef",
+		                "#063c5e"
+		            ],
+		            hoverBackgroundColor: [
+		                "#057dc8",
+		                "#d9e7ef",
+		                "#063c5e"
+		            ]
+		        }
+		    ]
+		};
+		var fact2 = new Chart(ctx2, {
+		    type: 'doughnut',
+		    data: data2,
+		    options: {
+	            responsive: true
+	        }
+		});
+		$('.modal-trigger').leanModal();
+		//$('.carousel').carousel({dist: 0});
+		
     }); // end of document ready
 })(jQuery); // end of jQuery name space
--- partner/static/partner/js/partner.js
+++ partner/static/partner/js/partner.js
@@ -5,6 +5,7 @@ function ajax_partner_carousel_items_list_append(selector){
     $.ajax('partner/carousel-items-list/', {
         success: function(carousel_inner_html){
             $(selector).append(carousel_inner_html)
+            $('.carousel').carousel({dist: 0});
         }
     })
 }
