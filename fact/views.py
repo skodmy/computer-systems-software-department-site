2d3b5ddf553d594bfb38efda609b35c57925c10b
Dmitro Skochinskiy <SkochinskiyDmitro@gmail.com>
2016-05-27 13:28:13 +0300

    still working on news

--- fact/views.py
+++ fact/views.py
@@ -5,11 +5,11 @@ from fact.models import Fact
 
 
 def rows(request):
-    return render(request, 'fact/rows.html', {'facts': Fact.objects.all()[:3]})
+    return render(request, 'fact/rows.html', {'facts': Fact.objects.all()[:4]})
 
 
 def facts_arguments_json(request):
     context = {}
-    for fact in Fact.objects.all()[:3]:
+    for fact in Fact.objects.all()[:4]:
         context.setdefault(fact.id, fact.arguments_json())
     return JsonResponse(context)
