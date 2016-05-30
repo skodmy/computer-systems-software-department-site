9f82c9206b5b9a55a8f1162bde0b452b85ca2e53
projero <ukrainetsua@gmail.com>
2016-05-30 07:42:04 +0300

    Advertisment-init.js and Advertisment-floating-block.js

---
+++ advertisement/static/advertisement/js/advertisments-init.js
@@ -0,0 +1,28 @@
+(function($){
+  $(function(){
+  	$('.nav-dropdown-button').dropdown({hover: true});
+  	$('.mobile-dropdown-button').dropdown({hover: false});
+  	$('#user-menu-btn').dropdown({belowOrigin: true, constrain_width : false, hover: false});
+  	$('#mobile-user-menu-btn').dropdown({belowOrigin: true, constrain_width : false, hover: false});
+    $('#mobile-nav-btn').sideNav();
+    $('#mobile-login-btn').sideNav();
+	Materialize.updateTextFields();
+
+	$('.modal-trigger').leanModal();
+
+	$(window).scroll(function(){
+		// Up button show and hide
+		if ($(this).scrollTop() > 200) {
+			$('#up_button').fadeIn();
+		} else {
+			$('#up_button').fadeOut();
+		}	
+	});
+	
+	//Click event to scroll to top
+	$('#up_button').click(function(){
+		$('html, body').animate({scrollTop : 0},800);
+		return false;
+	});
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
