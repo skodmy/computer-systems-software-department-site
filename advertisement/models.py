56d5b986e65a988dd9dc2d2832fa597f08a43878
Skochynskyi Dmytro <skochinskiydmitro@gmail.com>
2016-08-01 17:18:02 +0300

    fields author and views are added to Advertisement model

--- advertisement/models.py
+++ advertisement/models.py
@@ -6,7 +6,9 @@ class Advertisement(models.Model):
     main_message = models.TextField(max_length=300)
     published_date_time = models.DateTimeField(auto_now_add=True)
     is_actual = models.BooleanField(default=True)
-    image = models.ImageField(upload_to='advertisement/images', null=True)
+    image = models.ImageField(upload_to='advertisement/images', null=True, blank=True)
+    author = models.CharField(max_length=200, default='Анонім')
+    views = models.IntegerField(default=0)
 
     def __str__(self):
-        return self.title
+        return self.title + ' ' + self.author
