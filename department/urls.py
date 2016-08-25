b0e8734c6a2a8fe84bc1dd91ce28bffc70224e90
Alex Orlowskyy <rpgooyea@gmail.com>
2016-08-25 22:12:46 +0300

    Fix "slides" magic-bug.

--- department/urls.py
+++ department/urls.py
@@ -1,12 +1,11 @@
 from django.conf.urls import url
-from . views import index, ajax_login, manual_logout, slides, contacts, applicant_in_general, under_development, \
+from . views import index, ajax_login, manual_logout, contacts, applicant_in_general, under_development, \
     about_specialization
 
 urlpatterns = [
     url(r'^$', index),  # if something wrong with urls then don't forget to check this
     url(r'^login/$', ajax_login),
     url(r'^logout/$', manual_logout),
-    url(r'^slides/$', slides),
     url(r'^contacts/$', contacts),
     url(r'^applicant-in-general/$', applicant_in_general),
     url(r'^about-specialization/$', about_specialization),
