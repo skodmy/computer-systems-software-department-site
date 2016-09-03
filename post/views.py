e54152761afef72b031ad899aa2cd798249d02a5
Skochynskyi Dmytro <skochinskiydmitro@gmail.com>
2016-09-03 17:21:40 +0300

    single-articles viewing completed

--- post/views.py
+++ post/views.py
@@ -8,14 +8,21 @@ ADVERTISEMENTS_PER_PAGE = 8
 
 
 def create_paginator_context(model_class, objects_per_page, page_url, active_page_number=1):
-    context = {'paginator': Paginator(model_class.objects.all(), objects_per_page), 'page_url': page_url}
+    context = {
+        'paginator': Paginator(model_class.objects.all(), objects_per_page),
+        'page_url': page_url
+    }
     context.setdefault('active_page', context.get('paginator').page(active_page_number))
     return context
 
 
 def create_post_context(post_header, post_record_class, post_card_class, single_post_url):
-    return {'post_header4': post_header, 'post_record_class': post_record_class, 'post_card_class': post_card_class,
-            'single_post_url': single_post_url}
+    return {
+        'post_header4': post_header,
+        'post_record_class': post_record_class,
+        'post_card_class': post_card_class,
+        'single_post_url': single_post_url
+    }
 
 
 def create_row_posts_block_context(header, posts, single_url):
@@ -50,3 +57,22 @@ def advertisements(request, page_number):
                                  page_number))
     context.setdefault('authors_rating', Author.rating_objects())
     return render(request, 'post/advertisements.html', context)
+
+
+def news_single_article(request, article_id):
+    article = News.objects.get(id=article_id)
+    context = {'post': article}
+    context.update(create_row_posts_block_context('Останні новини', News.objects.all().order_by('-publication_date_time')[:5],'single-url'))
+    context.update(create_side_posts_block_context('Схожі новини',
+            News.objects.filter(tags=article.tags.all(),
+                publication_date_time=article.publication_date_time).exclude(id=article.id),
+    'single-url'))
+    return render(request, 'post/news_single_article.html', context)
+
+
+def advertisement(request, advertisement_id):
+    adv = Advertisement.objects.get(id=advertisement_id)
+    context = {'post': adv}
+    context.update(create_row_posts_block_context('Топ оголошення', Advertisement.objects.all().order_by('-views')[:5], 'single-url'))
+    context.update(create_side_posts_block_context('Інші оголошення цього автора', Advertisement.objects.filter(author=adv.author)[:5], 'single-url'))
+    return render(request, 'post/advertisment.html', context)
