109145b0e9e8cbf23ca6c60d1aa0e871534fb6c1
Alex Orlowskyy <rpgooyea@gmail.com>
2016-09-04 17:20:07 +0300

    Added URL to Technologies

--- technology/models.py
+++ technology/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class Technology(models.Model):
     name = models.CharField(max_length=75)
     logotype = models.ImageField(upload_to='technology/images')
+    site_url = models.URLField(default='http://localhost')
 
     def __str__(self):
         return self.name
